e5766db46d2025dc1a4aa89028c4775e4b3bde31
Nick Bennett <nick@nickbenn.com>
2020-02-03 11:51:19 -0700

    Display name.

--- test/edu/cnm/deepdive/PalindromesTest.java
+++ test/edu/cnm/deepdive/PalindromesTest.java
@@ -2,6 +2,7 @@ package edu.cnm.deepdive;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
@@ -9,12 +10,14 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 class PalindromesTest {
 
+  @DisplayName("Test known palindromes")
   @ParameterizedTest
   @CsvFileSource(resources = "/affirmative.csv")
   void isPalindromeAffirmative(String testCase) {
     assertTrue(Palindromes.isPalindrome(testCase));
   }
 
+  @DisplayName("Test known non-palindromes")
   @ParameterizedTest
   @CsvFileSource(resources = "/negative.csv")
   void isPalindromeNegative(String testCase) {
